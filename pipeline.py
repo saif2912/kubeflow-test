e6c8d8c3416bc6b9d9df37f61b83d583cbd2507e
saif2912 <54722799+saif2912@users.noreply.github.com>
2022-07-28 11:11:48 +0530

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -4,16 +4,16 @@ PIPELINE_NAME="pipelines"
 import os
 PROJECT_DIR=os.path.join(os.path.expanduser("~"),"imported",PIPELINE_NAME)
 print(PROJECT_DIR)
-!tfx template copy \
+tfx template copy \
   --pipeline-name={PIPELINE_NAME} \
   --destination-path={PROJECT_DIR} \
   --model=taxi
-!{sys.executable} -m models.features_test
-!{sys.executable} -m models.keras.model_test
-!gsutil cp data/data.csv gs://{GOOGLE_CLOUD_PROJECT}-kubeflowpipelines-default/tfx-template/data/taxi/data.csv
-!tfx pipeline create  --pipeline-path=kubeflow_runner.py --endpoint={ENDPOINT} \
+{sys.executable} -m models.features_test
+{sys.executable} -m models.keras.model_test
+gsutil cp data/data.csv gs://{GOOGLE_CLOUD_PROJECT}-kubeflowpipelines-default/tfx-template/data/taxi/data.csv
+tfx pipeline create  --pipeline-path=kubeflow_runner.py --endpoint={ENDPOINT} \
 --build-image
-!tfx run create --pipeline-name={PIPELINE_NAME} --endpoint={ENDPOINT}
+tfx run create --pipeline-name={PIPELINE_NAME} --endpoint={ENDPOINT}
 # Copyright 2020 Google LLC. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
