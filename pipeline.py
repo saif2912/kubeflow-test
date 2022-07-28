fafb4eb39a978f31f637b6ae14d0715d37e3a11a
saif2912 <54722799+saif2912@users.noreply.github.com>
2022-07-28 12:00:04 +0530

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -1,4 +1,20 @@
-# Copyright 2019 Google LLC. All Rights Reserved.
+import sys
+ENDPOINT='https://167fbc89e032e2bb-dot-us-central1.pipelines.googleusercontent.com'
+PIPELINE_NAME="pipelines"
+import os
+PROJECT_DIR=os.path.join(os.path.expanduser("~"),"imported",PIPELINE_NAME)
+print(PROJECT_DIR)
+!template copy \
+  --pipeline-name={PIPELINE_NAME} \
+  --destination-path={PROJECT_DIR} \
+  --model=taxi
+#{sys.executable} -m models.features_test
+#{sys.executable} -m models.keras.model_test
+gsutil cp data/data.csv gs://{GOOGLE_CLOUD_PROJECT}-kubeflowpipelines-default/tfx-template/data/taxi/data.csv
+tfx pipeline create  --pipeline-path=kubeflow_runner.py --endpoint={ENDPOINT} \
+--build-image
+tfx run create --pipeline-name={PIPELINE_NAME} --endpoint={ENDPOINT}
+# Copyright 2020 Google LLC. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,183 +27,202 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Chicago Taxi example using TFX DSL on Kubeflow."""
+"""TFX taxi template pipeline definition.
 
+This file defines TFX pipeline and various components in the pipeline.
+"""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+from typing import Text,Any, Dict, List, Optional
 
-import os
-from tfx.components.evaluator.component import Evaluator
-from tfx.components.example_gen.big_query_example_gen.component import BigQueryExampleGen
-from tfx.components.example_validator.component import ExampleValidator
-from tfx.components.model_validator.component import ModelValidator
-from tfx.components.pusher.component import Pusher
-from tfx.components.schema_gen.component import SchemaGen
-from tfx.components.statistics_gen.component import StatisticsGen
-from tfx.components.trainer.component import Trainer
-from tfx.components.transform.component import Transform
-from tfx.orchestration.kubeflow.runner import KubeflowRunner
-from tfx.orchestration.pipeline import PipelineDecorator
-from tfx.proto import evaluator_pb2
+import tensorflow_model_analysis as tfma
+from tfx import v1 as tfx
+
+from ml_metadata.proto import metadata_store_pb2
+from tfx.components import CsvExampleGen
+from tfx.components import Evaluator
+from tfx.components import ExampleValidator
+from tfx.components import Pusher
+# from tfx.components import ResolverNode
+from tfx.components import SchemaGen
+from tfx.components import StatisticsGen
+from tfx.components import Trainer
+from tfx.components import Transform
+from tfx.components.base import executor_spec
+from tfx.components.trainer import executor as trainer_executor
+from tfx.dsl.experimental import latest_blessed_model_resolver
+from tfx.extensions.google_cloud_ai_platform.pusher import executor as ai_platform_pusher_executor
+from tfx.extensions.google_cloud_ai_platform.trainer import executor as ai_platform_trainer_executor
+from tfx.extensions.google_cloud_big_query.example_gen import component as big_query_example_gen_component
+from tfx.orchestration import pipeline
 from tfx.proto import pusher_pb2
 from tfx.proto import trainer_pb2
-
-# Directory and data locations (uses Google Cloud Storage).
-_input_bucket = 'gs://my-bucket'
-_output_bucket = 'gs://my-bucket'
-_pipeline_root = os.path.join(_output_bucket, 'tfx')
-
-# Google Cloud Platform project id to use when deploying this pipeline.
-_project_id = 'my-gcp-project'
-
-# Python module file to inject customized logic into the TFX components. The
-# Transform and Trainer both require user-defined functions to run successfully.
-# Copy this from the current directory to a GCS bucket and update the location
-# below.
-_taxi_utils = os.path.join(_input_bucket, 'taxi_utils.py')
-
-# Path which can be listened to by the model server.  Pusher will output the
-# trained model here.
-_serving_model_dir = os.path.join(_output_bucket, 'serving_model/taxi_bigquery')
-
-# Region to use for Dataflow jobs and CMLE training.
-#   Dataflow: https://cloud.google.com/dataflow/docs/concepts/regional-endpoints
-#   CMLE:     https://cloud.google.com/ml-engine/docs/tensorflow/regions
-_gcp_region = 'us-central1'
-
-# A dict which contains the training job parameters to be passed to Google
-# Cloud ML Engine. For the full set of parameters supported by Google Cloud ML
-# Engine, refer to
-# https://cloud.google.com/ml-engine/reference/rest/v1/projects.jobs#Job
-_cmle_training_args = {
-    'pythonModule': None,  # Will be populated by TFX
-    'args': None,  # Will be populated by TFX
-    'region': _gcp_region,
-    'jobDir': os.path.join(_output_bucket, 'tmp'),
-    'runtimeVersion': '1.12',
-    'pythonVersion': '2.7',
-    'project': _project_id,
-}
-
-# A dict which contains the serving job parameters to be passed to Google
-# Cloud ML Engine. For the full set of parameters supported by Google Cloud ML
-# Engine, refer to
-# https://cloud.google.com/ml-engine/reference/rest/v1/projects.models
-_cmle_serving_args = {
-    'model_name': 'chicago_taxi',
-    'project_id': _project_id,
-    'runtime_version': '1.12',
-}
-
-# The rate at which to sample rows from the Chicago Taxi dataset using BigQuery.
-# The full taxi dataset is > 120M record.  In the interest of resource
-# savings and time, we've set the default for this example to be much smaller.
-# Feel free to crank it up and process the full dataset!
-_query_sample_rate = 0.001  # Generate a 0.1% random sample.
+from tfx.types import Channel
+from tfx.types.standard_artifacts import Model
+from tfx.types.standard_artifacts import ModelBlessing
+# from tfx.utils.dsl_utils import external_input
 
 
-# TODO(zhitaoli): Remove PipelineDecorator after 0.13.0.
-@PipelineDecorator(
-    pipeline_name='chicago_taxi_pipeline_kubeflow',
-    log_root='/var/tmp/tfx/logs',
-    pipeline_root=_pipeline_root,
-    additional_pipeline_args={
-        'beam_pipeline_args': [
-            '--runner=DataflowRunner',
-            '--experiments=shuffle_mode=auto',
-            '--project=' + _project_id,
-            '--temp_location=' + os.path.join(_output_bucket, 'tmp'),
-            '--region=' + _gcp_region,
-        ],
-        # Optional args:
-        # 'tfx_image': custom docker image to use for components.
-    })
-def _create_pipeline():
+def create_pipeline(
+    pipeline_name: str,
+    pipeline_root: str,
+    data_path: str,
+    # TODO(step 7): (Optional) Uncomment here to use BigQuery as a data source.
+    # query: str,
+    preprocessing_fn: str,
+    run_fn: str,
+    train_args: tfx.proto.TrainArgs,
+    eval_args: tfx.proto.EvalArgs,
+    eval_accuracy_threshold: float,
+    serving_model_dir: str,
+    schema_path: Optional[str] = None,
+    metadata_connection_config: Optional[
+        metadata_store_pb2.ConnectionConfig] = None,
+    beam_pipeline_args: Optional[List[str]] = None,
+    ai_platform_training_args: Optional[Dict[str, str]] = None,
+    ai_platform_serving_args: Optional[Dict[str, Any]] = None,
+) -> tfx.dsl.Pipeline:
   """Implements the chicago taxi pipeline with TFX."""
 
-  query = """
-          SELECT
-            pickup_community_area,
-            fare,
-            EXTRACT(MONTH FROM trip_start_timestamp) AS trip_start_month,
-            EXTRACT(HOUR FROM trip_start_timestamp) AS trip_start_hour,
-            EXTRACT(DAYOFWEEK FROM trip_start_timestamp) AS trip_start_day,
-            UNIX_SECONDS(trip_start_timestamp) AS trip_start_timestamp,
-            pickup_latitude,
-            pickup_longitude,
-            dropoff_latitude,
-            dropoff_longitude,
-            trip_miles,
-            pickup_census_tract,
-            dropoff_census_tract,
-            payment_type,
-            company,
-            trip_seconds,
-            dropoff_community_area,
-            tips
-          FROM `bigquery-public-data.chicago_taxi_trips.taxi_trips`
-          WHERE RAND() < {}""".format(_query_sample_rate)
+  components = []
 
   # Brings data into the pipeline or otherwise joins/converts training data.
-  example_gen = BigQueryExampleGen(query=query)
+  example_gen = tfx.components.CsvExampleGen(input_base=data_path)
+  # TODO(step 7): (Optional) Uncomment here to use BigQuery as a data source.
+  # example_gen = tfx.extensions.google_cloud_big_query.BigQueryExampleGen(
+  #     query=query)
+  components.append(example_gen)
 
   # Computes statistics over data for visualization and example validation.
-  statistics_gen = StatisticsGen(input_data=example_gen.outputs.examples)
+  statistics_gen = tfx.components.StatisticsGen(
+      examples=example_gen.outputs['examples'])
+  # TODO(step 5): Uncomment here to add StatisticsGen to the pipeline.
+  components.append(statistics_gen)
 
-  # Generates schema based on statistics files.
-  infer_schema = SchemaGen(stats=statistics_gen.outputs.output)
+  if schema_path is None:
+    # Generates schema based on statistics files.
+    schema_gen = tfx.components.SchemaGen(
+        statistics=statistics_gen.outputs['statistics'])
+    # TODO(step 5): Uncomment here to add SchemaGen to the pipeline.
+    components.append(schema_gen)
+  else:
+    # Import user provided schema into the pipeline.
+    schema_gen = tfx.components.ImportSchemaGen(schema_file=schema_path)
+    # TODO(step 5): (Optional) Uncomment here to add ImportSchemaGen to the
+    #               pipeline.
+    components.append(schema_gen)
 
-  # Performs anomaly detection based on statistics and data schema.
-  validate_stats = ExampleValidator(
-      stats=statistics_gen.outputs.output, schema=infer_schema.outputs.output)
+    # Performs anomaly detection based on statistics and data schema.
+    example_validator = tfx.components.ExampleValidator(  # pylint: disable=unused-variable
+        statistics=statistics_gen.outputs['statistics'],
+        schema=schema_gen.outputs['schema'])
+    # TODO(step 5): (Optional) Uncomment here to add ExampleValidator to the
+    #               pipeline.
+    components.append(example_validator)
 
   # Performs transformations and feature engineering in training and serving.
-  transform = Transform(
-      input_data=example_gen.outputs.examples,
-      schema=infer_schema.outputs.output,
-      module_file=_taxi_utils)
+  transform = tfx.components.Transform(
+      examples=example_gen.outputs['examples'],
+      schema=schema_gen.outputs['schema'],
+      preprocessing_fn=preprocessing_fn)
+  # TODO(step 6): Uncomment here to add Transform to the pipeline.
+  components.append(transform)
 
-  # Uses user-provided Python function that implements a model using TF-Learn.
-  trainer = Trainer(
-      module_file=_taxi_utils,
-      transformed_examples=transform.outputs.transformed_examples,
-      schema=infer_schema.outputs.output,
-      transform_output=transform.outputs.transform_output,
-      train_args=trainer_pb2.TrainArgs(num_steps=10000),
-      eval_args=trainer_pb2.EvalArgs(num_steps=5000),
-      custom_config={'cmle_training_args': _cmle_training_args})
+  # Uses user-provided Python function that implements a model.
+  trainer_args = {
+      'run_fn': run_fn,
+      'examples': transform.outputs['transformed_examples'],
+      'schema': schema_gen.outputs['schema'],
+      'transform_graph': transform.outputs['transform_graph'],
+      'train_args': train_args,
+      'eval_args': eval_args,
+  }
+  if ai_platform_training_args is not None:
+    trainer_args['custom_config'] = {
+        tfx.extensions.google_cloud_ai_platform.TRAINING_ARGS_KEY:
+            ai_platform_training_args,
+    }
+    trainer = tfx.extensions.google_cloud_ai_platform.Trainer(**trainer_args)
+  else:
+    trainer = tfx.components.Trainer(**trainer_args)
+  # TODO(step 6): Uncomment here to add Trainer to the pipeline.
+  components.append(trainer)
 
-  # Uses TFMA to compute a evaluation statistics over features of a model.
-  model_analyzer = Evaluator(
-      examples=example_gen.outputs.examples,
-      model_exports=trainer.outputs.output,
-      feature_slicing_spec=evaluator_pb2.FeatureSlicingSpec(specs=[
-          evaluator_pb2.SingleSlicingSpec(
-              column_for_slicing=['trip_start_hour'])
-      ]))
+  # Get the latest blessed model for model validation.
+  model_resolver = tfx.dsl.Resolver(
+      strategy_class=tfx.dsl.experimental.LatestBlessedModelStrategy,
+      model=tfx.dsl.Channel(type=tfx.types.standard_artifacts.Model),
+      model_blessing=tfx.dsl.Channel(
+          type=tfx.types.standard_artifacts.ModelBlessing)).with_id(
+              'latest_blessed_model_resolver')
+  # TODO(step 6): Uncomment here to add Resolver to the pipeline.
+  components.append(model_resolver)
 
-  # Performs quality validation of a candidate model (compared to a baseline).
-  model_validator = ModelValidator(
-      examples=example_gen.outputs.examples, model=trainer.outputs.output)
+  # Uses TFMA to compute a evaluation statistics over features of a model and
+  # perform quality validation of a candidate model (compared to a baseline).
+  eval_config = tfma.EvalConfig(
+      model_specs=[
+          tfma.ModelSpec(
+              signature_name='serving_default',
+              label_key='tips_xf',
+              preprocessing_function_names=['transform_features'])
+      ],
+      slicing_specs=[tfma.SlicingSpec()],
+      metrics_specs=[
+          tfma.MetricsSpec(metrics=[
+              tfma.MetricConfig(
+                  class_name='BinaryAccuracy',
+                  threshold=tfma.MetricThreshold(
+                      value_threshold=tfma.GenericValueThreshold(
+                          lower_bound={'value': eval_accuracy_threshold}),
+                      change_threshold=tfma.GenericChangeThreshold(
+                          direction=tfma.MetricDirection.HIGHER_IS_BETTER,
+                          absolute={'value': -1e-10})))
+          ])
+      ])
+  evaluator = tfx.components.Evaluator(
+      examples=example_gen.outputs['examples'],
+      model=trainer.outputs['model'],
+      baseline_model=model_resolver.outputs['model'],
+      # Change threshold will be ignored if there is no baseline (first run).
+      eval_config=eval_config)
+  # TODO(step 6): Uncomment here to add Evaluator to the pipeline.
+  components.append(evaluator)
 
   # Checks whether the model passed the validation steps and pushes the model
   # to a file destination if check passed.
-  pusher = Pusher(
-      model_export=trainer.outputs.output,
-      model_blessing=model_validator.outputs.blessing,
-      custom_config={'cmle_serving_args': _cmle_serving_args},
-      push_destination=pusher_pb2.PushDestination(
-          filesystem=pusher_pb2.PushDestination.Filesystem(
-              base_directory=_serving_model_dir)))
+  pusher_args = {
+      'model':
+          trainer.outputs['model'],
+      'model_blessing':
+          evaluator.outputs['blessing'],
+  }
+  if ai_platform_serving_args is not None:
+    pusher_args['custom_config'] = {
+        tfx.extensions.google_cloud_ai_platform.experimental
+        .PUSHER_SERVING_ARGS_KEY:
+            ai_platform_serving_args
+    }
+    pusher = tfx.extensions.google_cloud_ai_platform.Pusher(**pusher_args)  # pylint: disable=unused-variable
+  else:
+    pusher_args['push_destination'] = tfx.proto.PushDestination(
+        filesystem=tfx.proto.PushDestination.Filesystem(
+            base_directory=serving_model_dir))
+    pusher = tfx.components.Pusher(**pusher_args)  # pylint: disable=unused-variable
+  # TODO(step 6): Uncomment here to add Pusher to the pipeline.
+  components.append(pusher)
 
-  return [
-      example_gen, statistics_gen, infer_schema, validate_stats, transform,
-      trainer, model_analyzer, model_validator, pusher
-  ]
-
-
-pipeline = KubeflowRunner().run(_create_pipeline())
+  return tfx.dsl.Pipeline(
+      pipeline_name=pipeline_name,
+      pipeline_root=pipeline_root,
+      components=components,
+      # Change this value to control caching of execution results. Default value
+      # is `False`.
+      # enable_cache=True,
+      metadata_connection_config=metadata_connection_config,
+      beam_pipeline_args=beam_pipeline_args,
+  )
 
 compiler.Compiler().compile(pipeline_func=pipeline,
         package_path='kubeflow.zip')
